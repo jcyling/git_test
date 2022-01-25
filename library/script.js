ef7330cc8c10e7c63b9e502dc74a43b9f88c2d08
jcyling <jane.cy.ling@gmail.com>
2022-01-25 08:37:27 +0800

    Add local storage and submit form

--- library/script.js
+++ library/script.js
@@ -1,9 +1,27 @@
 let myLibrary = [];
 
+// Static Selectors
 const gallery = document.querySelector(".gallery");
 const addBtn = document.querySelector(".add");
+const submit = document.querySelector(".submit");
+const formData = document.getElementById("filmForm");
+addBtn.addEventListener("click", displayForm);
+submit.addEventListener("click", submitFilm(formData));
 
-addBtn.addEventListener("click", addFilmToLibrary);
+gallery.onclick = function(event) {
+    let target = event.target.closest('button');
+    if (!target) {
+        return;
+    }
+    let btnClass = target.classList;
+    if (btnClass.contains("remove")) {
+        removeFilm(target);
+    }
+    else if (btnClass.contains("watchState")) {
+        changeStatus(target);
+    }
+    saveStorage();
+}
 
 // Film constructor and prototype
 function Film (title, director, genre, watched) {
@@ -23,61 +41,73 @@ Film.prototype.info = function() {
     return `${this.title} by ${this.director}, ${this.genre} pages, ${this.watched}.`
 }
 
-// Existing entries
-const theHobbit = new Film ("The Hobbit", "Peter Jackson", "Fantasy", true);
-const StandByMe2 = new Film ("Stand By Me 2", "Takashi Yamazaki", "Fantasy", false);
+function submitFilm(formData) {
+    const titleField = formData.elements["title"];
+    const directorField = formData.elements["director"];
+    const title = titleField.value;
+    const director = directorField.value;
+    let genre;
 
-myLibrary.push(theHobbit);
-myLibrary.push(StandByMe2);
+    const genreSelects = document.querySelectorAll(`select[name="genre"]`);
+    for (const genreSelect of genreSelects) {
+        if(genreSelect.checked) {
+            genre = genreSelect.value;
+            console.log("genre");
+            break;
+        }
+    }
 
-// Add film to library
-function addFilmToLibrary() {
-    let title = prompt("Title");
-    let director = prompt("Director");
-    let genre = prompt("Genre");
-    let watched = prompt("Watched");
+    // const watched = prompt("Watched");
 
     if (!title || !director || !genre || !watched) {
         return window.alert("Check again!");
     }
     else {
         const newFilm = new Film(title, director, genre, watched);
+        myLibrary.push(newFilm);
+        let i = myLibrary.length;
+        let newCard = createFilm(newFilm, i);
+        gallery.appendChild(newCard);
+        saveStorage();
     }
-
-    myLibrary.push(newFilm);
 }
 
-// Display films on the page
 function displayFilms() {
     for (let i = 0; i < myLibrary.length; i++) {
-        let card = document.createElement("div");
-        let title = document.createElement("h3");
-        let director = document.createElement("h4");
-        let genre = document.createElement("h4");
-
-        let userControls = document.createElement("div");
-        let rmvBtn = document.createElement("button");
-        let watchBtn = document.createElement("button");
-
-        title.textContent = myLibrary[i].title;
-        director.textContent = myLibrary[i].director;
-        genre.textContent = myLibrary[i].genre;
-        watchBtn.textContent = watchedOrNot(myLibrary[i].watched);
-        rmvBtn.textContent = "Remove";
-
-        userControls.classList.add("userControls");
-        watchBtn.classList.add("watchState");
-        watchBtn.setAttribute("value", i);
-
-        rmvBtn.classList.add("remove");
-        rmvBtn.setAttribute("value", i);
-
-        card.classList.add("film");
-        card.setAttribute("id", i);
-        userControls.append(rmvBtn, watchBtn);
-        card.append(title, director, genre, userControls);
-        gallery.appendChild(card);
+        let newCard = createFilm(myLibrary[i], i);
+        gallery.appendChild(newCard);
     }
+}
+
+function createFilm(film, i) {
+    const card = document.createElement("div");
+    const title = document.createElement("h3");
+    const director = document.createElement("h4");
+    const genre = document.createElement("h4");
+
+    const userControls = document.createElement("div");
+    const rmvBtn = document.createElement("button");
+    const watchBtn = document.createElement("button");
+
+    title.textContent = film.title;
+    director.textContent = film.director;
+    genre.textContent = film.genre;
+    watchBtn.textContent = watchedOrNot(film.watched);
+    rmvBtn.textContent = "Remove";
+
+    userControls.classList.add("userControls");
+    watchBtn.classList.add("watchState");
+    watchBtn.setAttribute("value", i);
+
+    rmvBtn.classList.add("remove");
+    rmvBtn.setAttribute("value", i);
+
+    card.classList.add("film");
+    card.setAttribute("id", i);
+    userControls.append(rmvBtn, watchBtn);
+    card.append(title, director, genre, userControls);
+
+    return card;
 }
 
 function changeStatus(button) {
@@ -85,7 +115,6 @@ function changeStatus(button) {
     let status = myLibrary[index].watched;
 
     if (status) {
-        console.log('hello');
         status = false;
     }
     else {
@@ -101,23 +130,41 @@ function watchedOrNot(value) {
 }
 
 function removeFilm(button) {
-    console.log('hello');
     let index = button.value;
     myLibrary.splice(index, 1);
     let parentCard = document.getElementById(index);
+    console.log(parentCard);
     gallery.removeChild(parentCard);
 }
 
+// Form functions
+function displayForm() {
+    const visibilityForm = document.querySelector(".formContainer");
+    visibilityForm.style.visibility = "visible";
+}
+
+// Existing entries
+const theHobbit = new Film ("The Hobbit", "Peter Jackson", "Fantasy", true);
+const StandByMe2 = new Film ("Stand By Me 2", "Takashi Yamazaki", "Fantasy", false);
+
+// Initial display
+myLibrary = loadStorage();
 displayFilms();
 
-const rmvBtns = document.querySelectorAll(".remove");
-const watchBtns = document.querySelectorAll(".watchState");
+// Local storage
+function loadStorage() {
+    const library = JSON.parse(localStorage.getItem("myLibrary"));
+    if (library) {
+        console.log('Been here before');
+        return library;
+    }
+    else {
+        console.log('Hi new person!');
+        myLibrary.push(theHobbit);
+        myLibrary.push(StandByMe2);
+    }
+}
 
-rmvBtns.forEach((rmvBtn) => {
-    rmvBtn.addEventListener("click", () => removeFilm(rmvBtn));
-})
-
-watchBtns.forEach((watchBtn) => {
-    watchBtn.addEventListener("click", () => changeStatus(watchBtn));
-})
-
+function saveStorage() {
+    localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+}
